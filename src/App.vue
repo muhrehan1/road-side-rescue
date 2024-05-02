dd91947485b0a7c7c1ba0b6554476923a7b29c31
Arshman Fareed <arshmanthakurr555@gmail.com>
2024-05-02 20:33:04 +0500

    Project Setup

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Footer from './components/Footer.vue'
+</script>
+
+<template>
+  <div>
+    
+    <Header />
+    <Main />
+    <Footer />
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
